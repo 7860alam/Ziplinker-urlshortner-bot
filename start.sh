a6be291929fd144b1b82ce86a88ac6186e4a10db
7860alam <137377298+7860alam@users.noreply.github.com>
2023-11-08 14:02:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Nobideveloper/Shortner-Converter-Bot.git /Shortner-Converter-Bot
-cd /Shortner-Converter-Bot
+git clone https://github.com/7860alam/Ziplinker-urlshortner-bot.git /Ziplinker-urlshortner-bot
+cd /Ziplinker-urlshortner-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
